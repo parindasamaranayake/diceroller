a752ac6ea1a9c3e490c187d399eeb7abb5691969
Parinda Samaranayake <parindasamaranayake@proton.me>
2026-01-30 21:53:52 +0530

    Dark mode was added

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import random
 from tkinter import *
 
+global is_on
+is_on = True
+
 # Array
 numbers = [1, 2, 3, 4, 5, 6]
 
@@ -51,14 +54,35 @@ def roll():
 def retry():
     window.update()
     window.update_idletasks()
-    #answer()
     input_field.delete(0, "end")
     output_text.config(text="")
     output_image.config(image="")
 
+def dark_mode():
+    global is_on
+    if is_on:
+        window.config(background="black")
+        caption.config(background="black", foreground="white")
+        label.config(background="black", foreground="white")
+        answer_label.config(background="black", foreground="white")
+        output_text.config(background="black", foreground="white")
+        output_image.config(background="black", foreground="white")
+        dark_mode_button.config(text="Light Mode")
+        is_on = False
+    else:
+        window.config(background="white")
+        caption.config(background="white", foreground="black")
+        label.config(background="white", foreground="black")
+        answer_label.config(background="white", foreground="black")
+        output_text.config(background="white", foreground="black")
+        output_image.config(background="white", foreground="black")
+        dark_mode_button.config(text="Dark Mode")
+        is_on = True
+
 # Main window
 window = Tk()
 window.title("Dice Roller")
+window.configure(background="white")
 window.geometry("300x300")
 favicon = PhotoImage(file="Images/dice_favicon.png")
 window.iconphoto(True, favicon)
@@ -72,35 +96,40 @@ d5 = PhotoImage(file="Images/d5.png")
 d6 = PhotoImage(file="Images/d6.png")
 
 # Caption
-caption = Label(window, text="Dice Roller", font=("arial", 30, "bold"))
+caption = Label(window, text="Dice Roller", font=("arial", 30, "bold"), background="white")
 caption.pack()
 
 # "Enter your guess here" label
-label = Label(window, text="Enter your guess here", font=("arial", 12))
+label = Label(window, text="Enter your guess here", font=("arial", 12), background="white")
 label.pack()
 
 # Input entry box
-input_field = Entry(window, font=("arial", 10), width=5)
+input_field = Entry(window, font=("arial", 10), width=5, bd=3, relief="ridge")
+input_field.pack()
 input_field.place(x=116, y=75)
 
 # Roll button
 roll_button = Button(window, text="Roll", command=roll, width=5, height=2)
-roll_button.place(x=90, y=100)
+roll_button.place(x=60, y=100)
 
 # Retry button
 retry_button = Button(window, text="Retry", command=retry, width=5, height=2)
-retry_button.place(x=140, y=100)
+retry_button.place(x=110, y=100)
+
+# Dark mode button
+dark_mode_button = Button(window, text="Dark Mode", command=dark_mode, width=9, height=2)
+dark_mode_button.place(x=160, y=100)
 
 # Answer label
-answer_label = Label(window, text="Answer", font=("arial", 12))
+answer_label = Label(window, text="Answer", font=("arial", 12), background="white")
 answer_label.place(x=110, y=160)
 
 # Output text
-output_text = Label(window)
+output_text = Label(window, background="white")
 output_text.place(x=110, y=180)
 
 # Output image
-output_image = Label(window)
+output_image = Label(window, background="white")
 output_image.place(x=105, y=215)
 
 window.mainloop()
